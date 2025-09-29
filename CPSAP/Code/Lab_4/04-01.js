3c382630d697af1bd2b3153ac09c03ce577e2c36
Manmad <philmade6@gmail.com>
2025-09-29 09:20:38 +0300

    CPSAP: lab4 1st task in progress

--- CPSAP/Code/Lab_4/04-01.js
+++ CPSAP/Code/Lab_4/04-01.js
@@ -1,11 +1,51 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var events = require('events');
 var util = require('util');
+var data_module = require('./Db_module');
 const PORT = 5000;
 
+var db = new data_module.DB();
 
-const server = http.createServer();
+db.on('GET', (request, response) => {
+    console.log("DB.GET triggered");
+    response.writeHead(200,{'content-type':'application/json;charset=utf-8'});
+    response.end(JSON.stringify(db.get()))
+});
+
+db.on('POST', (request, response) => {
+    console.log("DB.POST triggered");
+    request.on('data',data=>{
+        let dat = JSON.parse(data);
+        db.post(dat);
+        response.end(JSON.stringify(dat));
+    });
+
+});
+db.on('DELETE',(request,response)=>{
+    console.log("DB.PUT triggered");
+    request.on('data',data=>{
+        let parsed = JSON.parse(data);
+        db.put(parsed);
+        response.end(JSON.stringify(db.get()));
+    });
+});
+
+
+
+
+const server = http.createServer(function (request,response){
+    if(url.parse(request.url).pathname==="/api/db"){
+        db.emit(request.method,request,response);
+    }
+    else{
+        response.writeHead(404,{'content-type':'text/html'});
+        response.end(
+            "<h1>404 Not Found</h1>"
+        );
+    }
+});
 
 server.listen(PORT);
 
--- CPSAP/Code/Lab_4/Db_module.js
+++ CPSAP/Code/Lab_4/Db_module.js
@@ -1,4 +1,27 @@
 var util = require('util');
 var events = require('events');
 
+var db_data = [
+    {id:1,name:"CPSAP"},
+    {id:2,name:"OS"},
+    {id:3,name:"SP"},
+    {id:4,name:"NAP"},
+    {id:5,name:"CGB"},
+    {id:6,name:"CMS"}
+];
 
+
+function DB(){
+    this.get = ()=>{return db_data;};
+    this.post = (data_piece)=>{db_data.push(data_piece);};
+    this.post = (to_update)=>{
+        const index = db_data.findIndex(item=>item.id==to_update.id);
+        if(index!=-1){
+            db_data[index] = {...db_data[index],...to_update.name};
+        }
+    };
+}
+
+util.inherits(DB,events.EventEmitter);
+
+exports.DB = DB;
