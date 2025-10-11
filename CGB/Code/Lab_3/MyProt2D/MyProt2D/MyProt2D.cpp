c3450a9ab82dfb0cb9d630072d1f67183bffe73d
Manmad <philmade6@gmail.com>
2025-10-11 10:00:19 +0300

    CGB: lab1, 2 3 done

---
+++ CGB/Code/Lab_3/MyProt2D/MyProt2D/MyProt2D.cpp
@@ -0,0 +1,187 @@
+﻿
+// MyProt2D.cpp: определяет поведение классов для приложения.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "afxwinappex.h"
+#include "afxdialogex.h"
+#include "MyProt2D.h"
+#include "MainFrm.h"
+
+#include "MyProt2DDoc.h"
+#include "MyProt2DView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMyProt2DApp
+
+BEGIN_MESSAGE_MAP(CMyProt2DApp, CWinApp)
+	ON_COMMAND(ID_APP_ABOUT, &CMyProt2DApp::OnAppAbout)
+	// Стандартные команды по работе с файлами документов
+	ON_COMMAND(ID_FILE_NEW, &CWinApp::OnFileNew)
+	ON_COMMAND(ID_FILE_OPEN, &CWinApp::OnFileOpen)
+END_MESSAGE_MAP()
+
+
+// Создание CMyProt2DApp
+
+CMyProt2DApp::CMyProt2DApp() noexcept
+{
+
+	// TODO: замените ниже строку идентификатора приложения строкой уникального идентификатора; рекомендуемый
+	// формат для строки: ИмяКомпании.ИмяПродукта.СубПродукт.СведенияОВерсии
+	SetAppID(_T("MyProt2D.AppID.NoVersion"));
+
+	// TODO: добавьте код создания,
+	// Размещает весь важный код инициализации в InitInstance
+}
+
+// Единственный объект CMyProt2DApp
+
+CMyProt2DApp theApp;
+
+
+// Инициализация CMyProt2DApp
+
+BOOL CMyProt2DApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+
+	// Инициализация библиотек OLE
+	if (!AfxOleInit())
+	{
+		AfxMessageBox(IDP_OLE_INIT_FAILED);
+		return FALSE;
+	}
+
+	AfxEnableControlContainer();
+
+	EnableTaskbarInteraction(FALSE);
+
+	// Для использования элемента управления RichEdit требуется метод AfxInitRichEdit2()
+	// AfxInitRichEdit2();
+
+	// Стандартная инициализация
+	// Если эти возможности не используются и необходимо уменьшить размер
+	// конечного исполняемого файла, необходимо удалить из следующего
+	// конкретные процедуры инициализации, которые не требуются
+	// Измените раздел реестра, в котором хранятся параметры
+	// TODO: следует изменить эту строку на что-нибудь подходящее,
+	// например на название организации
+	SetRegistryKey(_T("Локальные приложения, созданные с помощью мастера приложений"));
+	LoadStdProfileSettings(4);  // Загрузите стандартные параметры INI-файла (включая MRU)
+
+
+	// Зарегистрируйте шаблоны документов приложения.  Шаблоны документов
+	//  выступают в роли посредника между документами, окнами рамок и представлениями
+	CSingleDocTemplate* pDocTemplate;
+	pDocTemplate = new CSingleDocTemplate(
+		IDR_MAINFRAME,
+		RUNTIME_CLASS(CMyProt2DDoc),
+		RUNTIME_CLASS(CMainFrame),       // основное окно рамки SDI
+		RUNTIME_CLASS(CMyProt2DView));
+	if (!pDocTemplate)
+		return FALSE;
+	AddDocTemplate(pDocTemplate);
+
+
+	// Разрешить использование расширенных символов в горячих клавишах меню
+	CMFCToolBar::m_bExtCharTranslation = TRUE;
+
+	// Синтаксический разбор командной строки на стандартные команды оболочки, DDE, открытие файлов
+	CCommandLineInfo cmdInfo;
+	ParseCommandLine(cmdInfo);
+
+
+
+	// Команды диспетчеризации, указанные в командной строке.  Значение FALSE будет возвращено, если
+	// приложение было запущено с параметром /RegServer, /Register, /Unregserver или /Unregister.
+	if (!ProcessShellCommand(cmdInfo))
+		return FALSE;
+
+	// Одно и только одно окно было инициализировано, поэтому отобразите и обновите его
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+	return TRUE;
+}
+
+int CMyProt2DApp::ExitInstance()
+{
+	//TODO: обработайте дополнительные ресурсы, которые могли быть добавлены
+	AfxOleTerm(FALSE);
+
+	return CWinApp::ExitInstance();
+}
+
+// Обработчики сообщений CMyProt2DApp
+
+
+// Диалоговое окно CAboutDlg используется для описания сведений о приложении
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg() noexcept;
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+// Реализация
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTestsF1();
+	afx_msg void OnTestsF2();
+	afx_msg void OnTestsF3();
+};
+
+CAboutDlg::CAboutDlg() noexcept : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+	ON_COMMAND(ID_TESTS_F1, &CAboutDlg::OnTestsF1)
+	ON_COMMAND(ID_TESTS_F2, &CAboutDlg::OnTestsF2)
+	ON_COMMAND(ID_TESTS_F3, &CAboutDlg::OnTestsF3)
+END_MESSAGE_MAP()
+
+// Команда приложения для запуска диалога
+void CMyProt2DApp::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+// Обработчики сообщений CMyProt2DApp
+
+void CAboutDlg::OnTestsF1()
+{
+	Invalidate();
+}
+
+
+void CAboutDlg::OnTestsF2()
+{
+	Invalidate();
+}
+
+
+void CAboutDlg::OnTestsF3()
+{
+	Invalidate();
+}
---
+++ CGB/Code/Lab_3/MyProt2D/MyProt2D/MyProt2DDoc.cpp
@@ -0,0 +1,137 @@
+﻿
+// MyProt2DDoc.cpp: реализация класса CMyProt2DDoc 
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
+// и поиска; позволяет совместно использовать код документа в данным проекте.
+#ifndef SHARED_HANDLERS
+#include "MyProt2D.h"
+#endif
+
+#include "MyProt2DDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMyProt2DDoc
+
+IMPLEMENT_DYNCREATE(CMyProt2DDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CMyProt2DDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// Создание или уничтожение CMyProt2DDoc
+
+CMyProt2DDoc::CMyProt2DDoc() noexcept
+{
+	// TODO: добавьте код для одноразового вызова конструктора
+
+}
+
+CMyProt2DDoc::~CMyProt2DDoc()
+{
+}
+
+BOOL CMyProt2DDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: добавьте код повторной инициализации
+	// (Документы SDI будут повторно использовать этот документ)
+
+	return TRUE;
+}
+
+
+
+
+// Сериализация CMyProt2DDoc
+
+void CMyProt2DDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: добавьте код сохранения
+	}
+	else
+	{
+		// TODO: добавьте код загрузки
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Поддержка для эскизов
+void CMyProt2DDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Измените этот код для отображения данных документа
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Поддержка обработчиков поиска
+void CMyProt2DDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Задание содержимого поиска из данных документа.
+	// Части содержимого должны разделяться точкой с запятой ";"
+
+	// Например:  strSearchContent = _T("точка;прямоугольник;круг;объект ole;");
+	SetSearchContent(strSearchContent);
+}
+
+void CMyProt2DDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// Диагностика CMyProt2DDoc
+
+#ifdef _DEBUG
+void CMyProt2DDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CMyProt2DDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+// Команды CMyProt2DDoc
