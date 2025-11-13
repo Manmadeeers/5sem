eeae8423e5eb57171986dbb434650aef789268d5
Manmad <philmade6@gmail.com>
2025-11-13 10:53:42 +0300

    NAP:lab8 in progress. Text modified

---
+++ NAP/Code/Lab_8/ConcurrentServer/ConcurrentServer.cpp
@@ -0,0 +1,24 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <Windows.h>
+#include <string>	
+#pragma comment(lib,"WS2_32.lib")
+
+
+DWORD WINAPI AcceptServer(LPVOID pPrm) {
+	DWORD rc = 0;
+
+
+	ExitThread(0);
+}
+
+
+int main(int argc, char* argv[]) {
+
+
+
+	system("pause");
+	return 0;
+}
+
