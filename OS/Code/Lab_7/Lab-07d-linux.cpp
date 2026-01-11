77dce1c97db26ea44239f29d73943e5fa3c4d43c
Manmad <philmade6@gmail.com>
2026-01-11 23:50:21 +0300

    OS:lab-07d for linux modified

--- OS/Code/Lab_7/Lab-07d-linux.cpp
+++ OS/Code/Lab_7/Lab-07d-linux.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <cstring>
 #include <cstdlib>
+#include <limits.h>
 
 using namespace std;
 
@@ -19,10 +20,8 @@ void RunPrimeWorker(unsigned long long workSeconds)
 {
     timespec start{}, now{};
     clock_gettime(CLOCK_MONOTONIC, &start);
-
     unsigned long long primeCount = 0;
     unsigned long long number = 2;
-
     while (true)
     {
         if (IsPrime(number))
@@ -30,43 +29,35 @@ void RunPrimeWorker(unsigned long long workSeconds)
             cout << ++primeCount << ") " << number << endl;
         }
         number++;
-
         clock_gettime(CLOCK_MONOTONIC, &now);
         double elapsed =
             (now.tv_sec - start.tv_sec) +
             (now.tv_nsec - start.tv_nsec) / 1e9;
-
         if (elapsed >= workSeconds)
             break;
     }
-
     clock_gettime(CLOCK_MONOTONIC, &now);
     double total =
         (now.tv_sec - start.tv_sec) +
         (now.tv_nsec - start.tv_nsec) / 1e9;
-
     cout << "Worked for " << total << " seconds\n";
     cout << "Numbers found: " << primeCount << endl;
     cin.get();
 }
 
-void StartChildInNewTerminal(const char* seconds)
+void StartChildProcess(const char* exePath, const char* seconds)
 {
     pid_t pid = fork();
-
     if (pid == 0)
     {
-        execlp(
-            "xterm",
-            "xterm",
-            "-hold",
-            "-e",
-            "./Lab-07d",
-            seconds,
-            nullptr
-        );
-        perror("execlp");
-        exit(1);
+        // дочерний процесс: запустить тот же исполняемый файл с аргументом
+        execl(exePath, exePath, seconds, (char*)nullptr);
+        perror("execl");
+        _exit(127);
+    }
+    else if (pid < 0)
+    {
+        perror("fork");
     }
 }
 
@@ -78,12 +69,45 @@ int main(int argc, char* argv[])
         RunPrimeWorker(seconds);
         return 0;
     }
-    cout << "Launch child proccesses\n";
 
-    StartChildInNewTerminal("60");
-    StartChildInNewTerminal("120");
+    // Получаем абсолютный путь к исполняемому файлу.
+    // Если argv[0] уже абсолютный — используем его; иначе пытаемся получить realpath.
+    char exeFullPath[PATH_MAX];
+    if (argv[0][0] == '/')
+    {
+        strncpy(exeFullPath, argv[0], PATH_MAX-1);
+        exeFullPath[PATH_MAX-1] = '\0';
+    }
+    else
+    {
+        if (realpath(argv[0], exeFullPath) == nullptr)
+        {
+            // если realpath не сработал, попробуем сформировать из cwd + argv[0]
+            char cwd[PATH_MAX];
+            if (getcwd(cwd, sizeof(cwd)))
+            {
+                snprintf(exeFullPath, PATH_MAX, "%s/%s", cwd, argv[0]);
+            }
+            else
+            {
+                // fallback: просто argv[0]
+                strncpy(exeFullPath, argv[0], PATH_MAX-1);
+                exeFullPath[PATH_MAX-1] = '\0';
+            }
+        }
+    }
 
-    while (wait(nullptr) > 0);
+    cout << "Launch child processes\n";
+
+    StartChildProcess(exeFullPath, "60");
+    StartChildProcess(exeFullPath, "120");
+
+    // дождёмся всех детей
+    while (true)
+    {
+        pid_t w = wait(nullptr);
+        if (w == -1) break;
+    }
 
     cout << "All child processes finished\n";
     return 0;
